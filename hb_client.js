ba6832c72b9d7c87330f024ff5ae68aa6217b98e
matheus mendes <bigodines@yahoo.com.br>
2011-03-11 22:55:03 -0300

    typos and bad english

--- hb_client.js
+++
@@ -1,4 +0,0 @@
-var hb = require("./heartbeat.js");
-
-// start a client and start pulsing every 5 seconds 
-hb.client("127.0.0.1", 6688, 5000);
--- hb_server.js
+++
@@ -1,8 +0,0 @@
-var hb = require("./heartbeat.js");
-
-
-// start server with default values
-hb.server(); 
-
-// run!!
-setInterval(function() { hb.detector(7000); }, 2*1000);
